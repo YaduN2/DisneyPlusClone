526994b05f41d2136f34648798105623438575e3
Yadu Nandan <yadu.kunju@gmail.com>
2022-03-29 14:54:49 +0530

    ADD:Details

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import Details from "./components/Home/Details";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Home></Home>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/details" element={<Details />}></Route>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
