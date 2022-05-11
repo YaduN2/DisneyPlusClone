915c5ee83dc7864573e3b3cfc078cd6b7358379f
Yadu Nandan <yadu.kunju@gmail.com>
2022-05-11 17:31:02 +0530

    login

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Details from "./components/Home/Details";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Header />
         <Routes>
+          <Route path="/login" element={<Login />}></Route>
           <Route path="/details" element={<Details />}></Route>
           <Route path="/" element={<Home />}></Route>
         </Routes>
